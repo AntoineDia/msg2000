d0f11afaab324812ce40affcec4a0e43533ef5c2
AntoineDia <diambu.antoine@gmail.com>
2019-05-24 14:44:19 +0200

    Updates

---
+++ precache-manifest.63688397b984414aac73867b335859b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627ad5dad7a49e7bd483550550895606",
+    "url": "/msg2000/index.html"
+  },
+  {
+    "revision": "934f9a5499f03a60aeaf",
+    "url": "/msg2000/static/css/main.3a061e7d.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/msg2000/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "934f9a5499f03a60aeaf",
+    "url": "/msg2000/static/js/main.b63ef016.chunk.js"
+  },
+  {
+    "revision": "c00d9a596e5eba2f5b78",
+    "url": "/msg2000/static/js/runtime~main.efd46175.js"
+  }
+]);
